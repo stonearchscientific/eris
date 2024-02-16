ac8df9f16ca2eb01b4dd9a849503fa0d2cd40c1b
kennedycj <jacksonhaddock@gmail.com>
2024-02-15 18:56:13 -0600

    Add Python wrapper for AbstractContext.

--- concept.py
+++ concept.py
@@ -1,5 +1,7 @@
 from bitarray import bitarray, util
 from jpype import JClass
+
+
 class Concept:
     def bitarray_to_bitset(self, bit_array):
         bitset = JClass('java.util.BitSet')(len(bit_array))
@@ -7,41 +9,45 @@ class Concept:
             if bit_array[i]:
                 bitset.set(i)
         return bitset
+
     def bitset_to_bitarray(self, bitset):
         bit_array = bitarray(bitset.length())
         for i in range(bitset.length()):
             if bitset.get(i):
                 bit_array[i] = True
         return bit_array
+
     def __init__(self, extent, intent):
-        if isinstance(extent, bitarray):
-            java_extent = self.bitarray_to_bitset(extent)
-        else:
-            java_extent = extent
-        if isinstance(intent, bitarray):
-            java_intent = self.bitarray_to_bitset(intent)
-        else:
-            java_intent = intent
+        if not isinstance(extent, bitarray):
+            raise TypeError(f"Expected bitarray, got {type(extent)}")
+        if not isinstance(intent, bitarray):
+            raise TypeError(f"Expected bitarray, got {type(intent)}")
+        java_extent = self.bitarray_to_bitset(extent)
+        java_intent = self.bitarray_to_bitset(intent)
         self.java_concept = JClass('com.stonearchscientific.eris.Concept')(java_extent, java_intent)
+
     @property
     def extent(self):
-        if isinstance(self.java_concept.extent(), JClass('java.util.BitSet')):
-            return self.bitset_to_bitarray(self.java_concept.extent())
         return self.java_concept.extent()
+
     @property
     def intent(self):
-        if isinstance(self.java_concept.intent(), JClass('java.util.BitSet')):
-            return self.bitset_to_bitarray(self.java_concept.intent())
         return self.java_concept.intent()
+
     def __eq__(self, other):
         return self.java_concept.equals(other.java_concept)
+
     def __ne__(self, other):
         return not self.__eq__(other)
+
     def __le__(self, other):
         return self.java_concept.lessOrEqual(other.java_concept)
+
     def __ge__(self, other):
         return self.java_concept.greaterOrEqual(other.java_concept)
+
     def __str__(self):
         return str(self.java_concept.toString())
+
     def __repr__(self):
         return self.__str__()
--- eris_example.py
+++ eris_example.py
@@ -1,16 +1,17 @@
 import jpype.imports
 from jpype import JClass
+from context import Context
 import graphviz
 
-eris = './eris-core/target/eris-core-1.0.0.jar'
+eris = './eris-core/target/eris-core-1.0-SNAPSHOT.jar'
 blueprints = './eris-core/external/blueprints-core-2.6.0.jar'
 guava = './eris-core/external/guava-31.0.1-jre.jar'
 
 jpype.startJVM(classpath=[eris, blueprints, guava, 'classes'], convertStrings=False)
 
-from com.stonearchscientific.eris import Context, Matrix
+#from com.stonearchscientific.eris import Context, Matrix
 
-example = [
+relation = [
     [1, 1, 1, 1, 1],
     [1, 1, 1, 0, 1],
     [0, 0, 1, 1, 0],
@@ -18,15 +19,25 @@ example = [
     [1, 1, 1, 1, 0]
 ]
 
-relation = Matrix(example)
-objects = JClass('java.util.ArrayList')(["1", "2", "3", "4", "5"])
-attributes = JClass('java.util.ArrayList')(["a", "b", "c", "d", "e"])
-context = Context(objects, attributes, relation)
+#relation = Matrix(example)
+#objects = JClass('java.util.ArrayList')(["1", "2", "3", "4", "5"])
+#attributes = JClass('java.util.ArrayList')(["a", "b", "c", "d", "e"])
+context = Context(["1", "2", "3", "4", "5"], ["a", "b", "c", "d", "e"], relation)
 
-filename = 'eris-example-context.dot'
-f = open(filename, 'w', encoding='utf-8')
-f.write(str(context.graphviz()))
-f.close()
+#iterator = context.iterator()
+#while iterator.hasNext():
+#    print(f"Concept : {iterator.next()}")
 
-g = graphviz.Source.from_file(filename)
-g.view()
+for item in context.items():
+    print(f"java class : {type(item)}")
+    print(f"encoded extent: {item.extent()}")
+    print(f"encoded intent: {item.intent()}")
+    print(f"decoded concept : {context.decode(item)}")
+
+#filename = 'eris-example-context.dot'
+#f = open(filename, 'w', encoding='utf-8')
+#f.write(str(context.graphviz()))
+#f.close()
+
+#g = graphviz.Source.from_file(filename)
+#g.view()
